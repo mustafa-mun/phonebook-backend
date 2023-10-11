0d88fb5fb634b62c441632e36e04029350af06ab
mustafa <mustafamcm9@gmail.com>
2023-10-11 23:48:02 +0300

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("content", (req) => {
   return JSON.stringify(req.body);
@@ -90,7 +92,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
